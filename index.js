841407b78df320a0fad463e0e5b669a830cb6a2a
sjjsjsjakakaka <kbuorjonathan15@icloud.com>
2025-12-24 17:14:59 -0500

    Fix message event listener for WebSocket
    Refactor message handling to use the correct WebSocket instance.

--- index.js
+++ index.js
@@ -12,22 +12,13 @@ const wss = new WebSocket.Server({ port: PORT }, () => {
 wss.on('connection', (ws) => {
   console.log("A new client connected!");
   wsData.socket = ws;
-  wss.on("message", (msg) => {
+  ws.on("message", (msg) => {
     let parsedMsg = JSON.parse(msg);
     console.log(parsedMsg);
   });
 
   // Respond immediately
   ws.send("Welcome, client!");
-
-  // Listen for messages from client
-  ws.on('message', (message) => {
-    console.log("Received from client:", message);
-
-    // Echo the message back
-    ws.send(`Server received: ${message}`);
-  });
-
   // Handle client disconnection
   ws.on('close', () => {
     console.log("Client disconnected");

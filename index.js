51c1066742ae2904d3df89d28a546753dd575563
sjjsjsjakakaka <kbuorjonathan15@icloud.com>
2025-12-30 18:10:58 -0500

    Refactor WebSocket server implementation

--- index.js
+++ index.js
@@ -1,36 +1,7 @@
-import { WebSocketServer } from "ws";
-import http from "http";
+import WebSocket from "ws";
 
-const PORT = process.env.PORT || 8080;
+const ws = new WebSocket("wss://websocketbots-production-76f0.up.railway.app");
 
-// Create HTTP server (Railway requires this)
-const server = http.createServer();
-
-// Attach WebSocket server
-const wss = new WebSocketServer({ server });
-
-wss.on("connection", (ws, req) => {
-  console.log("Client connected:", req.socket.remoteAddress);
-
-  // Send a message when client connects
-  ws.send("Hello from Railway WebSocket server!");
-
-  ws.on("message", (message) => {
-    console.log("Received:", message.toString());
-
-    // Echo back
-    ws.send(`Server received: ${message}`);
-  });
-
-  ws.on("close", () => {
-    console.log("Client disconnected");
-  });
-
-  ws.on("error", (err) => {
-    console.error("WebSocket error:", err);
-  });
-});
-
-server.listen(PORT, () => {
-  console.log(`WebSocket server running on port ${PORT}`);
-});
+ws.on("open", () => console.log("Connected!"));
+ws.on("message", msg => console.log("Message:", msg.toString()));
+ws.on("error", err => console.log("Error:", err));
